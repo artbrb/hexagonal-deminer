13fb224c0babbf91baf71e2c85af0f1c028e7e35
Artem <artem.barabolenko@gmail.com>
2018-09-03 15:54:42 +0300

    added bypassing near hexagons, building a field

--- src/sample/Hexagon.java
+++ src/sample/Hexagon.java
@@ -4,6 +4,7 @@ import javafx.scene.input.MouseEvent;
 import javafx.scene.paint.Paint;
 import javafx.scene.shape.Circle;
 import javafx.scene.shape.Polygon;
+import javafx.util.Pair;
 
 import static java.lang.Math.PI;
 import static java.lang.StrictMath.*;
@@ -13,18 +14,17 @@ import static sample.Main.*;
 
 public class Hexagon extends Polygon {
     private static final int BLOCK_SIZE = 20;
-    private boolean isOpen = false;
-    private boolean isMine = false;
-    private boolean isFlag = false;
-    private int numbersOfBombsNear = 0;
-    private boolean bangMine;
+    public boolean isOpen = false;
+    public boolean isMine = false;
+    public boolean isFlag = false;
+    public int numbersOfBombsNear = 0;
+    private boolean bangMine = false;
     private int intermediateDistance;
     private int radius;
     public int columnСoordinate;
     public int rowCoordinate;
 //    public int rowCounterHexagons =  (int) ceil(500 / (radius * 2));
 //    public int columnCounterHexagons = (int) ceil(500 / (radius * 2 * sqrt(3)));
-
 
 
     Hexagon(int radius, int intermediateDistance) {
@@ -38,70 +38,56 @@ public class Hexagon extends Polygon {
         this.rowCoordinate = rowCoordinate;
 
 
-
     }
 
 
-    public  Polygon createHexagon(int column, int row, int radius, int intermediateDistance) {
-        Polygon hex = new Polygon();
-        Circle cir = new Circle();
+    public Polygon createHexagon(int row, int column, int radius, int intermediateDistance) {
+        Polygon hexagon = new Polygon();
 
         for (int i = 0; i < 6; i++) {
-
-            hex.getPoints().add(radius * sin(i * PI / 3));
-            hex.getPoints().add(radius * cos(i * PI / 3));
-
+            hexagon.getPoints().add(radius * sin(i * PI / 3));
+            hexagon.getPoints().add(radius * cos(i * PI / 3));
         }
 
         double halfHeight = sqrt(3) * radius / 2;
-        hex.setTranslateX((row * 2 - column % 2) * halfHeight + row * intermediateDistance + 36 );
-        hex.setTranslateY(column*(intermediateDistance + radius*1.5) + 36);
-        hex.setFill(Paint.valueOf("Gray"));
+        hexagon.setTranslateX((column * 2 - row % 2) * halfHeight + column * intermediateDistance + 36);
+        hexagon.setTranslateY(row * (intermediateDistance + radius * 1.5) + 36);
+        hexagon.setFill(Paint.valueOf("#666699"));
 
 
-        hex.setOnMousePressed((MouseEvent event) -> {
+        hexagon.setOnMousePressed((MouseEvent event) -> {
             if (!bangMine && !win) {
                 if (event.isPrimaryButtonDown()) {
-                    if (field[columnСoordinate][rowCoordinate].isNotOpen()) {
+                    if (isNotOpen()) {
                         openHexagons(columnСoordinate, rowCoordinate);
                         boolean check = countOpenedHexagons == ((SIZE_OF_FIELD * SIZE_OF_FIELD) - numberOfMines);
                         win = check;
                         if (bangAndLoss) {
-                            xCoordinateBang = columnСoordinate;
-                            yCoordinateBang = rowCoordinate;
+                            rowCoordinateBang = rowCoordinate;
+                            columnCoordinateBang = columnСoordinate;
                         }
                     }
-            }
-            if (event.isSecondaryButtonDown()) {
-                    field[columnСoordinate][rowCoordinate].invertFlag();
-            }
-            if (bangMine || win) {
+                }
+                if (event.isSecondaryButtonDown()) {
+                    invertFlag();
+                }
+                if (bangMine || win) {
                     restart();
-            }
+                }
 
             }
         });
 
-
-
-        cir.setRadius(50);
-        cir.setTranslateX((row * 2 - column % 2) * halfHeight + row * intermediateDistance + 36 );
-        cir.setTranslateY(column*(intermediateDistance + radius*1.5) + 36);
-        cir.setFill(Paint.valueOf("Blue"));
-
-
-        return hex;
+        return hexagon;
 
     }
 
 
-
-
-
     void openHexagon() {
-        isOpen = true;
+
+        if (!isMine) countOpenedHexagons++;
         bangMine = isMine;
-        countOpenedHexagons++;
+        isOpen = true;
     }
 
     boolean isNotOpen() {
@@ -116,13 +102,14 @@ public class Hexagon extends Polygon {
         return isMine;
     }
 
-    void invertFlag() {
-        isFlag = !isFlag;
+    public boolean invertFlag() {
+        return isFlag = !isFlag;
     }
 
-     void setCountBomb(int count) {
+    void setCountBomb(int count) {
         numbersOfBombsNear = count;
     }
+
     int getCountBomb() {
         return numbersOfBombsNear;
     }
@@ -131,16 +118,12 @@ public class Hexagon extends Polygon {
 
     }
 
-    public static Circle paintBomb(int x, int y) {
-        Circle circle = new Circle(x, y, 35);
-        circle.setFill(valueOf("Green"));
+
+    public static Circle paintingString(int row, int column) {
+        Circle circle = new Circle(row * 40, column * 40, 13);
+        circle.setFill(Paint.valueOf("Red"));
         return circle;
     }
-
-    void paintingString() {
-
-    }
-
 
 
 }
--- src/sample/Main.java
+++ src/sample/Main.java
@@ -6,25 +6,27 @@ import javafx.scene.Scene;
 import javafx.scene.paint.Paint;
 import javafx.scene.shape.Circle;
 import javafx.stage.Stage;
-import java.lang.reflect.Array;
+import javafx.util.Pair;
+
 import java.util.ArrayList;
 import java.util.List;
-
-
+import java.util.Random;
 
 
 public class Main extends Application {
+    public static List<Pair<Integer,Integer>> movesAround = new ArrayList<>();
     public boolean bangMine;
     public static int countOpenedHexagons;
-    final String FLAG_ICON = "flag";
+    final String FLAG_ICON = "F";
     public static int SIZE_OF_FIELD = 20;
-    public static int numberOfMines = 20;
-    public static int xCoordinateBang;
-    public static int yCoordinateBang;
+    public static int numberOfMines = 10;
+    public static int rowCoordinateBang;
+    public static int columnCoordinateBang;
     public static Hexagon[][] field = new Hexagon[SIZE_OF_FIELD][SIZE_OF_FIELD];
-    List list = new ArrayList<Integer>();
     public static boolean win = false;
     public static boolean bangAndLoss = false;
+    public int countBombs = 0;
+    Random random = new Random();
 
 
 
@@ -35,22 +37,14 @@ public class Main extends Application {
         primaryStage.setTitle("hexagonal deminer");
         Scene scene = new Scene(root, 800, 800);
         primaryStage.setScene(scene);
-        scene.setFill(Paint.valueOf("Blue"));
+        scene.setFill(Paint.valueOf("#648f6e"));
 
-        for (int column = 0; column < SIZE_OF_FIELD; column++) {
-            for (int row = 0; row < SIZE_OF_FIELD; row++) {
+        Circle circle = new Circle(6 * 40, 6 * 40, 13);
+        circle.setFill(Paint.valueOf("Red"));
 
-                Hexagon hex = new Hexagon(20, 2);
-                root.getChildren().add(hex.createHexagon(column, row, 20, 2));
-                field[column][row] = hex;
-                hex.columnСoordinate =column;
-                hex.rowCoordinate =row;
+        createField(root);
+        root.getChildren().add(circle);
 
-
-                root.getChildren().add(hex);
-
-            }
-        }
 
 
         primaryStage.setResizable(false);
@@ -64,11 +58,73 @@ public class Main extends Application {
         launch(args);
     }
 
-    public static void openHexagons(int column, int row) {
+    public void createField(Group root) {
+        for (int row = 0; row < SIZE_OF_FIELD; row++) {
+            for (int column = 0; column < SIZE_OF_FIELD; column++) {
+
+                Hexagon hexagon = new Hexagon(20, 2);
+                root.getChildren().add(hexagon.createHexagon(row, column, 20, 2));
+                field[row][column] = hexagon;
+                hexagon.rowCoordinate = row;
+                hexagon.columnСoordinate = column;
+
+
+
+                root.getChildren().add(hexagon);
+
+            }
+        }
+
+        while (countBombs < numberOfMines) {
+            int x;
+            int y;
+            do {
+                 x = random.nextInt(SIZE_OF_FIELD);
+                 y = random.nextInt(SIZE_OF_FIELD);
+            } while (field[x][y].getStatusMined());
+            field[x][y].setMineStatus();
+            countBombs++;
+        }
+
+
+        hexagonsAround();
+        //Обход поля
+        for (int row = 0; row < SIZE_OF_FIELD; row++)
+            for (int column = 0; column < SIZE_OF_FIELD; column++)
+                //проверка на отсутствие бомбы
+                if (!field[row][column].getStatusMined())
+                    //перебор по соседним шестиугольникам
+                    for (Pair<Integer, Integer> move : movesAround)
+                        //проверка на возможность хода
+                        if (checkOutOfField(row, column, move))
+                            //считаем ,если бомба
+                            if (field[row + move.getKey()][column + move.getValue()].getStatusMined())
+                                field[row][column].numbersOfBombsNear++;
+
+
+
+
 
     }
-    public void checkOutOfField() {
 
+    public static void openHexagons(int row, int column) {
+
+    }
+
+    public static void hexagonsAround() {
+        movesAround.add(new Pair<>(-1, -1));
+        movesAround.add(new Pair<>(-1, 0));
+        movesAround.add(new Pair<>(0, -1));
+        movesAround.add(new Pair<>(0, +1));
+        movesAround.add(new Pair<>(+1, -1));
+        movesAround.add(new Pair<>(+1, 0));
+    }
+
+    public boolean checkOutOfField( int row, int column, Pair<Integer, Integer> coordinates) {
+        int incrementRow = row + coordinates.getKey();
+        int incrementColumn = column + coordinates.getValue();
+        return 0 <= incrementRow && incrementRow < SIZE_OF_FIELD &&
+                0 <= incrementColumn && incrementColumn < SIZE_OF_FIELD;
     }
 
     public static void restart() {
